66658e720c6fdf335176c8446d863021e671917d
Sonia Pamela Juarez <spjuarez13591@gmail.com>
2022-05-01 21:08:14 -0300

    Incorporar arrays

---
+++ JavaScript/arrays.js
@@ -0,0 +1,16 @@
+//Creo un objeto canciones
+
+const canciones = [
+    { id: "0", titulo: "Don't Stop Me Now", artista: "Queen", album: "Jazz", duracion:"3:29", img: "https://placeimg.com/200/200/any" },
+    { id: "1", titulo: "I Want To Break Free", artista: "Queen", album: "Bohemian Rhapsody", duracion:"3:43", img: "https://placeimg.com/200/200/any" },
+    { id: "2", titulo: "Love Of My Life", artista: "Queen", album: "A Night At The Opera", duracion:"3:37", img: "https://placeimg.com/200/200/any" },
+    { id: "3", titulo: "Somebody To Love", artista: "Queen", album: "Bohemian Rhapsody", duracion:"4:55", img: "https://placeimg.com/200/200/any" },
+    { id: "4", titulo: "Bohemian Rhapsody", artista: "Queen", album: "Bohemian Rhapsody", duracion:"5:54", img: "https://placeimg.com/200/200/any" },
+    { id: "5", titulo: "Killer Queen", artista: "Queen", album: "Bohemian Rhapsody", duracion:"2:59", img: "https://placeimg.com/200/200/any" },
+    { id: "6", titulo: "Crazy little Thing Called Rock", artista: "Queen", album: "The Game", duracion:"2:43", img: "https://placeimg.com/200/200/any" },
+    { id: "7", titulo: "Under Pressure", artista: "Queen", album: "Hot Space", duracion:"4:08", img: "https://placeimg.com/200/200/any" },
+    { id: "8", titulo: "Radio Ga Ga", artista: "Queen", album: "The Works", duracion:"5:48", img: "https://placeimg.com/200/200/any" },
+    { id: "9", titulo: "The Show Most Go On", artista: "Queen", album: "Bohemian Rhapsody", duracion:"4:31", img: "https://placeimg.com/200/200/any" },
+    { id: "10", titulo: "A Kind Of Magic", artista: "Queen", album: "A Kind Of Magic", duracion:"4:24", img: "https://placeimg.com/200/200/any" },
+    { id: "11", titulo: "The Miracle", artista: "Queen", album: "The Miracle", duracion:"3:26", img: "https://placeimg.com/200/200/any" },
+];
