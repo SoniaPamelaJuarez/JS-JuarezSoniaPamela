5f8675289b2010a7fd9b032b5df0197f54267dcf
Sonia Pamela Juarez <spjuarez13591@gmail.com>
2022-05-23 01:28:09 -0300

    Segunda Entrega

--- JavaScript/segundaEntrega.js
+++ JavaScript/segundaEntrega.js
@@ -2,18 +2,18 @@
 
 //Creo un objeto canciones
 const canciones = [
-    { id: "0", titulo: "Don't Stop Me Now", artista: "Queen", album: "Jazz", duracion:"3:29", img: "https://placeimg.com/200/200/any" },
-    { id: "1", titulo: "I Want To Break Free", artista: "Queen", album: "Bohemian Rhapsody", duracion:"3:43", img: "https://placeimg.com/200/200/any" },
-    { id: "2", titulo: "Love Of My Life", artista: "Queen", album: "A Night At The Opera", duracion:"3:37", img: "https://placeimg.com/200/200/any" },
-    { id: "3", titulo: "Somebody To Love", artista: "Queen", album: "Bohemian Rhapsody", duracion:"4:55", img: "https://placeimg.com/200/200/any" },
-    { id: "4", titulo: "Bohemian Rhapsody", artista: "Queen", album: "Bohemian Rhapsody", duracion:"5:54", img: "https://placeimg.com/200/200/any" },
-    { id: "5", titulo: "Killer Queen", artista: "Queen", album: "Bohemian Rhapsody", duracion:"2:59", img: "https://placeimg.com/200/200/any" },
-    { id: "6", titulo: "Crazy little Thing Called Rock", artista: "Queen", album: "The Game", duracion:"2:43", img: "https://placeimg.com/200/200/any" },
-    { id: "7", titulo: "Under Pressure", artista: "Queen", album: "Hot Space", duracion:"4:08", img: "https://placeimg.com/200/200/any" },
-    { id: "8", titulo: "Radio Ga Ga", artista: "Queen", album: "The Works", duracion:"5:48", img: "https://placeimg.com/200/200/any" },
-    { id: "9", titulo: "The Show Most Go On", artista: "Queen", album: "Bohemian Rhapsody", duracion:"4:31", img: "https://placeimg.com/200/200/any" },
-    { id: "10", titulo: "A Kind Of Magic", artista: "Queen", album: "A Kind Of Magic", duracion:"4:24", img: "https://placeimg.com/200/200/any" },
-    { id: "11", titulo: "The Miracle", artista: "Queen", album: "The Miracle", duracion:"3:26", img: "https://placeimg.com/200/200/any" },
+    { id: "0", titulo: "Don't Stop Me Now", artista: "Queen", album: "Jazz", duracion:"3:29", img: "https://placeimg.com/200/200/people" },
+    { id: "1", titulo: "I Want To Break Free", artista: "Queen", album: "Bohemian Rhapsody", duracion:"3:43", img: "https://placeimg.com/200/200/animals" },
+    { id: "2", titulo: "Love Of My Life", artista: "Queen", album: "A Night At The Opera", duracion:"3:37", img: "https://placeimg.com/200/200/nature" },
+    { id: "3", titulo: "Somebody To Love", artista: "Queen", album: "Bohemian Rhapsody", duracion:"4:55", img: "https://placeimg.com/200/200/tech" },
+    { id: "4", titulo: "Bohemian Rhapsody", artista: "Queen", album: "Bohemian Rhapsody", duracion:"5:54", img: "https://placeimg.com/200/200/architecture" },
+    { id: "5", titulo: "Killer Queen", artista: "Queen", album: "Bohemian Rhapsody", duracion:"2:59", img: "https://placeimg.com/200/200/people" },
+    { id: "6", titulo: "Crazy little Thing Called Rock", artista: "Queen", album: "The Game", duracion:"2:43", img: "https://placeimg.com/200/200/nature" },
+    { id: "7", titulo: "Under Pressure", artista: "Queen", album: "Hot Space", duracion:"4:08", img: "https://placeimg.com/200/200/tech" },
+    { id: "8", titulo: "Radio Ga Ga", artista: "Queen", album: "The Works", duracion:"5:48", img: "https://placeimg.com/200/200/animals" },
+    { id: "9", titulo: "The Show Most Go On", artista: "Queen", album: "Bohemian Rhapsody", duracion:"4:31", img: "https://placeimg.com/200/200/people" },
+    { id: "10", titulo: "A Kind Of Magic", artista: "Queen", album: "A Kind Of Magic", duracion:"4:24", img: "https://placeimg.com/200/200/animals" },
+    { id: "11", titulo: "The Miracle", artista: "Queen", album: "The Miracle", duracion:"3:26", img: "https://placeimg.com/200/200/nature" },
 ];
 
 const contenedorMusica = document.getElementById("contenedorMusica");
@@ -28,13 +28,13 @@ const carritoDescarga = [];
 
 for(const cancion of canciones){
     //Creo los elementos del HTML, le agrego la clase para darle estilo
-    const artCard = document.createElement("article");
+    const artCard = document.createElement("figure");
     artCard.className = "post-card";
 
     const imgCard = document.createElement("img");
     imgCard.src = cancion.img; //le agrego el contenido de la imagen que se encuentra en el objeto canciones
 
-    const tituloCard = document.createElement("h1");
+    const tituloCard = document.createElement("figcaption");
     tituloCard.append(cancion.titulo);
 
     const artistaCard = document.createElement("h2");
@@ -138,4 +138,19 @@ const validarFormulario = (e) => {
 
 
 form.addEventListener("submit", validarFormulario);
+
+//Voy a crear un buscador de canciones (buscador de filtros)
+const d = document;
+
+function buscadorCancion(input, selector){
+    d.addEventListener("keyup", e =>{
+        if(e.target.matches(input)){
+            //console.log(e.key);
+            //console.log(e.target.value);
+            d.querySelectorAll(selector).forEach((el) => el.textContent.toLowerCase().includes(e.target.value)?el.classList.remove("filter"): el.classList.add("filter"));
+        }
+    });
+}
+
+buscadorCancion(".card_filter", ".post-card");
 
